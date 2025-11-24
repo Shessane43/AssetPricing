35ad1f4c529bcbd6d609f9f6c3c0b0e89908a035
Armand VINCENT <armand.vincent77@gmail.com>
2025-11-24 16:54:56 +0100

    bond

---
+++ bond.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Bond:
+    def __init__(self, nominal, coupon_rate, rate, maturity, frequency=1):
+        self.nominal = nominal
+        self.coupon_rate = coupon_rate
+        self.rate = rate
+        self.maturity = maturity
+        self.frequency = frequency
+    
+    def price(self):
+        price = 0.0
+        coupon = self.nominal * self.coupon_rate / self.frequency
+        n_periods = int(self.maturity * self.frequency)
+        
+        for i in range(1, n_periods + 1):
+            price += coupon / (1 + self.rate / self.frequency) ** i
+        
+        price += self.nominal / (1 + self.rate / self.frequency) ** n_periods
+        return price
+    
+    def cash_flows(self):
+        """Retourne une liste des cash flows et des temps (en années)"""
+        coupon = self.nominal * self.coupon_rate / self.frequency
+        n_periods = int(self.maturity * self.frequency)
+        times = np.arange(1/self.frequency, self.maturity + 1/self.frequency, 1/self.frequency)
+        flows = [coupon] * n_periods
+        flows[-1] += self.nominal  # ajout du nominal au dernier flux
+        return times, flows
+    
+    def plot_value_evolution(self):
+        """Trace l'évolution de la valeur actualisée de l'obligation dans le temps"""
+        times, flows = self.cash_flows()
+        remaining_value = []
+        
+        for t in times:
+            # actualisation des cash flows restant à chaque date
+            idx = times >= t
+            value = sum([f / (1 + self.rate / self.frequency) ** ((time - t) * self.frequency)
+                         for f, time in zip(np.array(flows)[idx], times[idx])])
+            remaining_value.append(value)
+        
+        plt.figure(figsize=(8,5))
+        plt.plot(times, remaining_value, marker='o')
+        plt.xlabel('Temps (années)')
+        plt.ylabel('Valeur actualisée des cash flows restants')
+        plt.title('Évolution de la valeur de l\'obligation')
+        plt.grid(True)
+        plt.show()
