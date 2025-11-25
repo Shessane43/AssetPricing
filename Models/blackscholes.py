7e5b076d9ddc55735710fdedd258e4edd0a01344
Soukayna Hessane <s.hessane43@gmail.com>
2025-11-25 11:28:19 +0100

    bs

--- Models/blackscholes.py
+++ Models/blackscholes.py
@@ -4,7 +4,7 @@ from Models.models import Model
 
 
 class BlackScholes(Model):
-    def __init__(self, S, K, r, sigma, T, option_type="call", position="buy", option_class="vanille"):
+    def __init__(self, S, K, r, sigma, T, option_type="Call", position="Buy", option_class="Vanille"):
         self.S = S
         self.K = K
         self.r = r
@@ -15,21 +15,21 @@ class BlackScholes(Model):
         self.option_class = option_class.lower()  # "vanille" ou "exotique"
 
     def price(self):
-        if self.option_class != "vanille":
+        if self.option_class != "Vanille":
             raise ValueError("Black-Scholes ne s'applique qu’aux options vanilles européennes.")
 
         d1 = (np.log(self.S / self.K) + (self.r + 0.5 * self.sigma**2) * self.T) / (self.sigma * np.sqrt(self.T))
         d2 = d1 - self.sigma * np.sqrt(self.T)
 
-        if self.option_type == "call":
+        if self.option_type == "Call":
             price = self.S * norm.cdf(d1) - self.K * np.exp(-self.r * self.T) * norm.cdf(d2)
         else:
             price = self.K * np.exp(-self.r * self.T) * norm.cdf(-d2) - self.S * norm.cdf(-d1)
 
-        return -price if self.position == "sell" else price
+        return -price if self.position == "Sell" else price
 
     def implied_volatility(self, market_price, tol=1e-6, max_iter=100):
-        if self.option_class != "vanille":
+        if self.option_class != "Vanille":
             raise ValueError("La volatilité implicite n'est définie que pour les options vanilles.")
 
         sigma = 0.2  # initial guess
@@ -37,7 +37,7 @@ class BlackScholes(Model):
             d1 = (np.log(self.S / self.K) + (self.r + 0.5 * sigma**2) * self.T) / (sigma * np.sqrt(self.T))
             d2 = d1 - sigma * np.sqrt(self.T)
 
-            if self.option_type == "call":
+            if self.option_type == "Call":
                 price_est = self.S * norm.cdf(d1) - self.K * np.exp(-self.r * self.T) * norm.cdf(d2)
             else:
                 price_est = self.K * np.exp(-self.r * self.T) * norm.cdf(-d2) - self.S * norm.cdf(-d1)
