16beaeaa28ea059dac6010d845043e27e5dedf02
Tom Catoire <tomcatoire@mac.home>
2026-01-04 18:21:23 +0100

    modifs

--- pages/greeks.py
+++ pages/greeks.py
@@ -6,12 +6,13 @@ from functions.greeks_function import Greeks
 def app():
 
     # Vérification des paramètres
-    required = ["S","K","T","r","sigma","option_type","buy_sell","model_name","option_class"]
+    required = ["ticker","S","K","T","r","sigma","option_type","buy_sell","model_name","option_class"]
     if not all(k in st.session_state for k in required):
         st.error("Certains paramètres manquent dans la session.")
         return
 
     try:
+        ticker =st.session_state["ticker"]
         S = float(st.session_state["S"])
         K = float(st.session_state["K"])
         T = float(st.session_state["T"])
@@ -26,6 +27,24 @@ def app():
     buy_sell = str(st.session_state["buy_sell"])
     option_class = str(st.session_state["option_class"])
     model_name = str(st.session_state["model_name"])
+
+    with st.container(border=True):
+        st.markdown(
+            f"""
+            **Ticker** : **{ticker}**
+
+            **Spot (S)** : {S:.4f} &nbsp;&nbsp;|&nbsp;&nbsp;
+            **Strike (K)** : {K:.4f} &nbsp;&nbsp;|&nbsp;&nbsp;
+            **Maturité (T)** : {T} an(s)
+
+
+            **Taux sans risque (r)** : {r:.2%} &nbsp;&nbsp;|&nbsp;&nbsp;
+            **Volatilité (σ)** : {sigma:.2%} &nbsp;&nbsp;|&nbsp;&nbsp;
+            **Dividendes (q)** : {q:.2%} 
+
+            **Type d'option** : **{option_type}**
+            """
+        )
 
     st.subheader(f"Modèle choisi : {model_name}")
 
